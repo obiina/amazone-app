b46a412ba99b58a6eee9ff6dcf6b75ccbd78dd58
Obinna Chimezie <obinna932@gmail.com>
2021-01-12 04:27:07 +0400

    Node updates

---
+++ backend/data.js
@@ -0,0 +1,65 @@
+const data = {
+    products:[
+        {
+            _id: '1',
+            name:'Nike Slim Shirts',
+            category: 'Shirts',
+            image:'/image/image.jpg',
+            price:'100',
+            countInStock:20,
+            brand:'Nike',
+            rating:3.5,
+            numReviews: 10,
+            description:'High Quality Products'
+        },
+        {
+            _id: '2',
+            name:'Addidas Slim Shirts',
+            category: 'Shirts',
+            image:'/image/image.jpg',
+            price:'300',
+            brand:'Nike',
+            rating:2.5,
+            countInStock:2,
+            numReviews: 10,
+            description:'High Quality Products'
+        },
+        {
+            _id: '3',
+            name:'Gucci Slim Shirts',
+            category: 'Shirts',
+            image:'/image/image.jpg',
+            price:'200',
+            countInStock:30,
+            brand:'Nike',
+            rating:4,
+            numReviews: 12,
+            description:'High Quality Products'
+        },
+        {
+            _id: '4',
+            name:'Puma Slim Shirts',
+            category: 'Shirts',
+            image:'/image/image.jpg',
+            price:'200',
+            countInStock:60,
+            brand:'Nike',
+            rating:4.5,
+            numReviews: 10,
+            description:'High Quality Products'
+        },
+        {
+            _id: '5',
+            name:'Doshe Slim Shirts',
+            category: 'Shirts',
+            image:'/image/image.jpg',
+            price:'200',
+            countInStock:90,
+            brand:'Nike',
+            rating:4.5,
+            numReviews: 10,
+            description:'High Quality Products'
+        },
+    ],
+};
+export default data;
